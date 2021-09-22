8fbbeed22342469080729e02b0f526daaebedcad
Matthew <matthewjo30@gmail.com>
2021-09-21 20:48:07 -0400

    Added more things to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Random from './Random';
 
 function Home() {
 
     return (
-        <div>
-            hello
+        <div className="container mx-auto ">
+            <p className="text-center">Welcome to my submission for the Winter 2022 Front End Shopify Challenge</p>
+            <p className="text-center">Please try to search for a specific earth date, or press the random button and get a set of random photos!</p>
+        
+            <Random />
         </div>
     );
 }

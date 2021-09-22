a3bd846c4f1408aa8f802d87366128de0c53be06
Matthew <matthewjo30@gmail.com>
2021-09-21 20:48:50 -0400

    Add margin to home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,8 +4,8 @@ import Random from './Random';
 function Home() {
 
     return (
-        <div className="container mx-auto ">
-            <p className="text-center">Welcome to my submission for the Winter 2022 Front End Shopify Challenge</p>
+        <div className="container mx-auto">
+            <p className="text-center mb-2">Welcome to my submission for the Winter 2022 Front End Shopify Challenge</p>
             <p className="text-center">Please try to search for a specific earth date, or press the random button and get a set of random photos!</p>
         
             <Random />
